b4ccb876bef3d164e259e1ebf31264668884e08e
webdevmasum <masumislam0018@gmail.com>
2025-05-14 19:58:01 +0600

    js basic math practice

---
+++ math/script.js
@@ -0,0 +1,11 @@
+
+
+// 🎯 Basic Example: Temperature Round
+
+
+let temperature = 23.7;  // Temperature in Celsius
+
+// Round the temperature to the nearest integer
+let roundedTemperature = Math.round(temperature);
+
+console.log("Rounded Temperature:", roundedTemperature);  // Output: 24
